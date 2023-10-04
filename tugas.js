5265fa83d9bf155933a32a4e97837cffeec8e3cf
rahmanrizaluddin <114675284+rahmanrizaluddin@users.noreply.github.com>
2023-10-04 12:36:59 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,9 @@
+const profile = {
+    nama : "Rahman Rizaluddin",
+    nim : 20220040295,
+    prodi : "teknik informatika",
+    alamat : "Kadudampit",
+    ttl : "sukabumi 16-03-2003",
+}
+console.log( `Halo, nama saya ${profile.nama} dan nim saya ${profile.nim} prodi ${profile.prodi}` );
+console.log( `rumah saya di ${profile.alamat} tempat tanggal lahir saya ${profile.ttl}`);
