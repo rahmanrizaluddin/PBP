d3147669a7d49a135204d3893b7d04de96d4a6eb
rahmanrizaluddin <114675284+rahmanrizaluddin@users.noreply.github.com>
2023-10-04 12:18:11 +0700

    Add files via upload

---
+++ assigment.js
@@ -0,0 +1,6 @@
+const favorites = [ "seafood", "salad", "Nugget", "Soup"];
+let myFood = "ice Cream";
+let herFood = "Noodles";
+[myFood, herFood] = favorites;
+console.log(myFood);
+console.log(herFood);
---
+++ projek3.js
@@ -0,0 +1,10 @@
+var a = 1;
+var b = 2;
+var temp;
+console.log("sebelum swap");
+console.log("nilai a : " + a);
+console.log("nilai b : " + b);
+[a, b] = [b, a] // menetapkan nilai a dengan nilai b dan nilai b dengan nilai a
+console.log("sebelum swap");
+console.log("nilai a : " + a);
+console.log("nilai b : " + b);
